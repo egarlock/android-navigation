c313de04fe8cf94125bdbef0a23fc9f395837f75
Eric Garlock <edg234@sandhills.com>
2020-09-03 08:45:44 -0500

    - added base fragment and activity

---
+++ app/src/main/java/com/egarlock/androidnavigation/ui/base/BaseActivity.kt
@@ -0,0 +1,30 @@
+package com.egarlock.androidnavigation.ui.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity() {
+
+    // region - Variables
+    // endregion
+
+
+
+    // region - Constructors
+    // endregion
+
+
+
+    // region - Activity Lifecycle Methods
+    // endregion
+
+
+
+    // region - Private API
+    // endregion
+
+
+
+    // region - Public API
+    // endregion
+
+}
---
+++ app/src/main/java/com/egarlock/androidnavigation/ui/base/BaseFragment.kt
@@ -0,0 +1,30 @@
+package com.egarlock.androidnavigation.ui.base
+
+import androidx.fragment.app.Fragment
+
+class BaseFragment : Fragment() {
+
+    // region - Variables
+    // endregion
+
+
+
+    // region - Constructors
+    // endregion
+
+
+
+    // region - Fragment Lifecycle Methods
+    // endregion
+
+
+
+    // region - Private API
+    // endregion
+
+
+
+    // region - Public API
+    // endregion
+
+}
